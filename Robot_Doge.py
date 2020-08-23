35f40e8b5222fec9118e3354212cca24dcda3f9a
tommar42 <35568112+tommar42@users.noreply.github.com>
2020-08-23 15:50:02 +0100

    Comment update
    Forgot to update the comment to say what command does

--- Robot_Doge.py
+++ Robot_Doge.py
@@ -79,7 +79,7 @@ class MyClient(discord.Client):
             await message.channel.send("Days since last major update: " + Days + " (Updates with just cases don't cout)")
             print("Tf2 was used by", message.author.name, "ID:", message.author.id)
 
-        #Simple test to see if bot is up
+        #Rolls a "dice" and sends result to user
         if message.content == 'dice':
             min = 1
             max = 6
